de1e7c454f6c9e7df4abd86fb54aefc30c4c83ca
John Buckley <John.buckley@mail.dcu.ie>
2016-10-13 21:49:15 +0100

    Assign1

---
+++ Assign1JohnBuckley2016/app/src/main/java/com/example/john/assign1johnbuckley2016/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.john.assign1johnbuckley2016;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void showToast (View view)
+    {
+        CharSequence text = getResources().getString(R.string.mess_toast);
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(this, text, duration);
+        toast.show();
+    }
+}
+
